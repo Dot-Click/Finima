b6dd4058b5419eec965d30e2604eabcf682a02d7
umer-2001 <104222183+ZubairArif234@users.noreply.github.com>
2024-12-10 14:41:51 +0500

    update the count checking condition

--- src/pages/EmployeeManagement.jsx
+++ src/pages/EmployeeManagement.jsx
@@ -217,6 +217,8 @@ const EmployeeManagement = () => {
     ],
     []
   );
+  console.log(employees);
+
   return (
     <div>
       <Tabs
@@ -324,7 +326,7 @@ const EmployeeManagement = () => {
             <div className="h-[60vh] flex justify-center items-center">
               <Loader type="dots" size="xl" color="dark" />
             </div>
-          ) : employees?.data?.length > 0 ? (
+          ) : employees?.totalDoc[0]?.count > 0 ? (
             <div className="mt-4">
               <CommonDataTable
                 data={employees?.data || []}
